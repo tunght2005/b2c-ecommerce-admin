ee53942f6a4f0b0edcf322c32736b8d481b2b384
Tung DEV <tunght2005@gmail.com>
2026-04-12 22:23:02 +0700

    refactor: restructure app layout and routing system

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './app/App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AuthProvider } from './contexts/app.context'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>
 )
---
+++ src/useRouteElement.tsx
@@ -0,0 +1,66 @@
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import path from './constants/path'
+import { useAuth } from './contexts/app.context'
+import NotFound from './pages/NotFound/NotFound'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Login from './pages/Login/Login'
+import MyProfile from './pages/MyProfile'
+
+//Tạo Protect để bảo vệ web khi user biêt domain mà chưa đăng nhập thì những cái như profile, apply k cho phép truy cập
+function ProtectedRoute() {
+  const { isAuthenticated } = useAuth()
+  return isAuthenticated ? <Outlet /> : <Navigate to={path.login} replace />
+}
+// // Chặn User vào lại trang login khi đã login rồi sẽ Navigate lại trang chủ
+function RejectedRoute() {
+  const { isAuthenticated } = useAuth()
+  return !isAuthenticated ? <Outlet /> : <Navigate to={path.dashboard} replace />
+}
+
+export default function useRouteElement() {
+  const routeElements = useRoutes([
+    {
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: path.dashboard,
+          element: (
+            <MainLayout>
+              <Dashboard />
+            </MainLayout>
+          )
+        },
+        {
+          path: path.myProfile,
+          element: (
+            <MainLayout>
+              <MyProfile />
+            </MainLayout>
+          )
+        }
+      ]
+    },
+    {
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: path.login,
+          index: true,
+          element: <Login />
+        }
+      ]
+    },
+    {
+      path: '*',
+      element: (
+        <MainLayout>
+          <NotFound />
+        </MainLayout>
+      )
+    }
+  ])
+  return routeElements
+}
